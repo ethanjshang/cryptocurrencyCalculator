c68fe2be7c3e2eae93f998ad4a530cc003885e2d
Ethan Shang <34875955+ethanjshang@users.noreply.github.com>
2018-02-01 10:30:29 -0800

    Major overhaul
    Fixed bugs in calculations and expanded output. Increased flexibility of price per cryptocurrency.

--- cryptoCalculator.py
+++ cryptoCalculator.py
@@ -1,11 +1,14 @@
 """
 TODO:
-Clarify distinction between amount_spent and amount_after fees
-Add test cases to make sure this program is working correctly
+Add Litecoin and bitcoin
 """
-"Calculator of Stock and Cryptocurrency"
-
 import sys
+"Calculator of Stock and Cryptocurrency"
+print()
+current_price_eth = float(input("Current price of ethereum: "))
+current_price_bitcoin = float(input("Current price of bitcoin: "))
+infile = open(sys.argv[1],"r")
+
 class cryptocurrency:
 	def __init__(self,amount_spent, price_at_purchase):
 		self.amount_spent = amount_spent
@@ -16,29 +19,18 @@ class cryptocurrency:
 		self.price_at_purchase = price_at_purchase
 		self.weighted_gain = 0
 		self.value_gain = 0
-"""
-class Stock:
-	def __init__(self, amount_spent, price_at_purchase,fees):
-		self.amount_spent = amount_spent
-		self.amount_after_fees = amount_spent - 4.99
-	def update_amount(self,new_amount):
-		self.amount_spent = new_amount
-class ETF(Stock):
-	type = ''
-
-"""
-class Ethereum(Cryptocurrency):
+class Ethereum(cryptocurrency):
 	type = 'eth'
-class Bitcoin(Cryptocurrency):
+	price = current_price_eth
+class Bitcoin(cryptocurrency):
 	type = 'btc'
+	price = current_price_bitcoin
 types_of_crypto = {
 	'eth' : Ethereum,
 	'btc' : Bitcoin
 }
 
-print()
-current_price_eth = float(input("Current price of ethereum: "))
-infile = open(sys.argv[1],"r")
+
 #Reading in info from the file
 line_info = []
 for line in infile:
@@ -56,17 +48,28 @@ for line in line_info:
 num_purchases = len(purchases)
 total_value_gain = 0
 total_spent = 0
+total_spent_after_fees = 0
 
 print()
 
 for purchase in purchases:
 	total_spent += purchase.amount_spent
-	purchase.weighted_gain = current_price_eth / purchase.price_at_purchase  * purchase.amount_after_fees
+	total_spent_after_fees += purchase.amount_after_fees
+	purchase.weighted_gain = purchase.price / purchase.price_at_purchase  * purchase.amount_after_fees
 	purchase.value_gain = purchase.weighted_gain - purchase.amount_after_fees
 	total_value_gain += purchase.value_gain
 	print("Change in investment value due to $" + str(purchase.amount_spent), "of", purchase.type, "bought at", purchase.price_at_purchase, ":", round(purchase.value_gain,2))
-print("\nCost Basis  :", total_spent)
-print("Market Value:", round(total_value_gain + total_spent,2))
-print("Total Absolute Change in investment value (including fees):", round(total_value_gain,2))
-print("Total Relative Change in investment value (including fees):", round((total_value_gain+total_spent)/total_spent,4))
+market_value = total_value_gain + total_spent_after_fees
+sell_price = round(market_value - (market_value * 0.0149),2)
+overall_change = round(market_value - (market_value * 0.0149)-total_spent,2)
+print()
+print("Total Spent Before Fees:", total_spent)
+print(" Total Spent After Fees:", total_spent_after_fees)
+print("     Total Value Change:", round(total_value_gain,2))
+print("           Market Value:", round(market_value,2),"\n")
 
+print("Total Absolute Change in investment value (after fees):", round(market_value-total_spent,2))
+print("Total Relative Change in investment value (after fees):", round(market_value/total_spent,4))
+
+print("\nAmount Gained if Sold (including sell fee):", sell_price)
+print("                            Overall Change:", overall_change)
